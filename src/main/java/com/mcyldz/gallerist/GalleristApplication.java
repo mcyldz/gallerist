152fc3482e83e52dfa5c7b2f5bb95d715e6158e5
mcyldz <116563621+mcyldz@users.noreply.github.com>
2025-02-10 22:16:37 +0300

    gallerist

--- src/main/java/com/mcyldz/gallerist/GalleristApplication.java
+++ src/main/java/com/mcyldz/gallerist/GalleristApplication.java
@@ -2,7 +2,13 @@ package com.mcyldz.gallerist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan(basePackages = {"com.mcyldz"})
+@EntityScan(basePackages = {"com.mcyldz"})
+@EnableJpaRepositories(basePackages = {"com.mcyldz"})
 @SpringBootApplication
 public class GalleristApplication {
 

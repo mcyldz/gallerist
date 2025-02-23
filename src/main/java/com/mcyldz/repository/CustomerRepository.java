7803bb65931023bb305d1753487004c8db770ab5
mcyldz <116563621+mcyldz@users.noreply.github.com>
2025-02-23 22:46:11 +0300

    Add Customer repository

---
+++ src/main/java/com/mcyldz/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.mcyldz.repository;
+
+import com.mcyldz.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}

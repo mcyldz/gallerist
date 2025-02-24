d0957df718685ee4c5e5667c57ec4875f9edbae1
mcyldz <116563621+mcyldz@users.noreply.github.com>
2025-02-24 23:13:03 +0300

    Add Gallerist repository

---
+++ src/main/java/com/mcyldz/repository/GalleristRepository.java
@@ -0,0 +1,9 @@
+package com.mcyldz.repository;
+
+import com.mcyldz.model.Gallerist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GalleristRepository extends JpaRepository<Gallerist, Long> {
+}

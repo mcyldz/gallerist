ec6aefb8eef8543137732575a34d2b46689a2741
mcyldz <116563621+mcyldz@users.noreply.github.com>
2025-02-27 20:49:09 +0300

    Add SaledCar repository

---
+++ src/main/java/com/mcyldz/repository/SaledCarRepository.java
@@ -0,0 +1,9 @@
+package com.mcyldz.repository;
+
+import com.mcyldz.model.SaledCar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaledCarRepository extends JpaRepository<SaledCar, Long> {
+}
